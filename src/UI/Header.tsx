dc35733cbb5ac4cd171894a4312a7b000719e7d5
tarook331@gmail.com <tarook331@gmail.com>
2024-06-26 15:23:07 +0300

    Edit Header styling

--- src/UI/Header.tsx
+++ src/UI/Header.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import UserAvatar from "./UserAvatar";
 import HeaderMenu from "./HeaderMenu";
+import Heading from "./Heading";
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
@@ -14,6 +15,7 @@ function Header() {
   return (
     <StyledHeader>
       <UserAvatar/>
+      <Heading as="h1"> Bink Index </Heading>
       <HeaderMenu/>
     </StyledHeader>
   );
--- src/UI/UserAvatar.tsx
+++ src/UI/UserAvatar.tsx
@@ -1,16 +1,42 @@
 import styled from "styled-components";
 const Avatar = styled.img`
   display: block;
+  margin-left: 2rem;
   width: 4rem;
-  width: 3.6rem;
   aspect-ratio: 1;
   object-fit: cover;
   object-position: center;
   border-radius: 50%;
   outline: 2px solid var(--color-grey-100);
 `;
+const Stacked = styled.div<{ $type?: string }>`
+  display: flex;
+  flex-direction: column;
+  gap: 0.2rem;
+
+  & span:first-child {
+    font-weight: 500;
+    justify-items: center;
+  }
+
+  & span:last-child {
+    color: var(--color-grey-500);
+    font-size: 1.2rem;
+  }
+`;
 function UserAvatar() {
-  const fullName ="Tarek AlHabbal ";
-  return <Avatar src={"/Tarek.jpg"} alt={`Avatar of ${fullName}`}></Avatar>;
+  const fullName = "Tarek AlHabbal";
+  return (
+    <>
+      <Stacked>
+        <span>
+          <Avatar src={"/Tarek.jpg"} alt={`Avatar of ${fullName}`} />
+        </span>
+        <span>
+          {"Tarek AlHabbal"}
+        </span>
+      </Stacked>
+    </>
+  );
 }
 export default UserAvatar;
